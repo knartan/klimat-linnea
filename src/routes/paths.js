8a61f8ecc9644c03b14a787770a81054e9c80592
nawicon <chao5_88@hotmail.com>
2022-12-04 21:19:07 +0100

    routing

---
+++ src/routes/paths.js
@@ -0,0 +1,11 @@
+const startViewPath ='/';
+const temperaturViewPath ='/temperatur';
+const quizViewPath ='/quiz';
+
+
+export default {
+    startViewPath,
+    temperaturViewPath,
+    quizViewPath,
+
+}
